a7f14a3ec84d1a26505d249d6f163b276d574997
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-01 14:40:17 +1000

    Update settings.py
    mixed error from incorrect indention

--- settings.py
+++ settings.py
@@ -3,10 +3,8 @@ class Settings:
 
 	def __init__(self):
 		"""Initialize the game's settings."""
-
-		def __init__(self):
-			# Screen settings
-			self.screen_width = 1200
-			self.screen_height = 800
-			self.bg_color = (230, 230, 230)
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
 			
