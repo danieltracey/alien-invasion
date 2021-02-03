59be2ff4df44d856d7d2f83cef26a09e72eba3b7
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-03 22:04:50 +1000

    The _update_screen() Method

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,10 +23,7 @@ class AlienInvasion:
 		"""Start the main loop for the game."""
 		while True:
 			self._check_events()
-
-			# Redraw the screen during each pass through the loop.
-			self.screen.fill(self.settings.bg_color)
-			self.ship.blitme()
+			self._update_screen()
 
 	def _check_events(self):
 		"""Respond to keypresses and mouse events."""
@@ -35,6 +32,11 @@ class AlienInvasion:
 				sys.exit()
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
+
+	def _update_screen(self):
+		"""Update images on the screen, and flip to the new screen."""
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
 
 if __name__ == '__main__':
 	# Make a game instance and run the game.
