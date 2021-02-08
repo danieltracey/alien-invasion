f802970fcd6ec0ef5f2a80bda8db903d31cbabaa
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-08 12:38:44 +1000

    Moving Both Left and Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,9 +34,15 @@ class AlienInvasion:
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RIGHT:
 					self.ship.moving_right = True
+				elif event.key == pygame.K_LEFT:
+					self.ship.moving_left = True
+
+
 			elif event.type == pygame.KEYUP:
 				if event.key == pygame.K_RIGHT:
 					self.ship.moving_right = False 
+				elif event.key == pygame.K_LEFT:
+					self.ship.moving_left = False 
 					# Move the ship to the right.
 					self.ship.rect.x += 1 
 			# Make the most recently drawn screen visible.
