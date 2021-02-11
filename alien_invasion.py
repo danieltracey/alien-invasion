9ceb956e62ea575d642be338987924eda5b265c7
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-11 11:42:05 +1000

    Firing Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behaviour."""
@@ -46,7 +47,9 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
-			sys.exit()	
+			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def	_check_keyup_events(self, event):
 		"""Respond to key releases."""
@@ -54,6 +57,11 @@ class AlienInvasion:
 			self.ship.moving_right = False 
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False 
+
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bllets group."""
+		new_bullet = Bullet(self)
+		self.bullets.add(new_bullet)
 		
 		# Move the ship to the right.
 		self.ship.rect.x += 1 
@@ -65,6 +73,8 @@ class AlienInvasion:
 		"""Update images on the screen, and flip to the new screen."""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 if __name__ == '__main__':
 	# Make a game instance and run the game.
