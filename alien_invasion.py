092d6ec0b6da67b6c6940bd1952e2f0c0ee58925
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-11 16:38:53 +1000

    Creating the_update_bullets() Method

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,14 +26,7 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
-
-			# Get rid of bullets that have disappeared.
-			for bullet in self.bullets.copy():
-				if bullet.rect.bottom <= 0:
-					self.bullets.remove(bullet)
-			print(len(self.bullets))
-			
+			self._update_bullets()
 			self._update_screen()
 
 	def _check_events(self):
@@ -64,11 +57,22 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False 
 
+
 	def _fire_bullet(self):
 		"""Create a new bullet and add it to the bllets group."""
 		if len(self.bullets) < self.settings.bullets_allowed:
 			new_bullet = Bullet(self)
 			self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets."""
+		# Update bullet positions
+		self.bullets.update()
+
+		# Get rid of bullets that have disappeared.
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
 
 
 	def _update_screen(self):
