3376261d3a146709433e5b94b1a01b28a5e637ee
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-12 10:31:47 +1000

    Checking Whether an Alien Has Hit the Edge

--- settings.py
+++ settings.py
@@ -22,6 +22,9 @@ class Settings:
 
 		# Alien settings 
 		self.alien_speed = 1.0
+		self.fleet_drop_speed = 10
+		# fleet_direction of 1 represenets right; -1 represents left.
+		self.fleet_direction = 1
 
 		
 			
