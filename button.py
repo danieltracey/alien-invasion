cf0e32deea40b6ea2f8204f2f20acbf602638af9
danieltracey <78135435+danieltracey@users.noreply.github.com>
2021-02-17 18:49:30 +1000

    Drawing the Button to the Screen

--- button.py
+++ button.py
@@ -30,6 +30,6 @@ class Button:
 	def draw_button(self):
 		# Draw blank button and then draw message.
 		self.screen.fill(self.button_color, self.rect)
-		self,screen.blit(self.msg_image, self.msg_image_rect)
+		self.screen.blit(self.msg_image, self.msg_image_rect)
 
 
